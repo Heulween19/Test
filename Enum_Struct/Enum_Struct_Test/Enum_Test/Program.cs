d6736c10fb534f20bac5ccae878a64b5d263c88d
Heulween19 <103297323+Heulween19@users.noreply.github.com>
2022-11-23 14:03:31 +0700

    add enum

---
+++ Enum_Struct/Enum_Struct_Test/Enum_Test/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum_Test
+{
+    internal class Program
+    { enum Month
+        {
+            January=1,    //0
+            February,  //1 
+            March,      //2
+            April,      //3
+            May,        //4
+            June,       //5
+            July,       //6
+            August,     //7
+            September,   //8
+            October,     //9
+            November,   //10
+            December    //11
+
+        }
+        static void Main(string[] args)
+        { 
+        Console.InputEncoding=Encoding.UTF8;
+        Console.OutputEncoding=Encoding.UTF8;
+            int eMonth;
+            Console.WriteLine("Nhap so thang");
+            eMonth = int.Parse(Console.ReadLine());
+            
+            switch (eMonth)
+            {
+                case 1:
+                    Console.WriteLine(Month.January);
+                    break;
+                case 2:
+                    Console.WriteLine(Month.February);
+                    break;
+                case 3:
+                    Console.WriteLine(Month.March);
+                    break;
+                default:
+                    Console.WriteLine("Sai dinh dang");
+                    break;
+
+            }
+
+            int eMonth1;
+            Console.WriteLine("Nhap so thang");
+            eMonth1 = int.Parse(Console.ReadLine());
+            if (eMonth1 == 1)
+            {
+                Console.WriteLine("Ban vua chon thang: " + (Month)1);
+            }
+
+
+            if (eMonth1 == (int)Month.February)
+            {
+                Console.WriteLine("Ban vua chon thang: " + (Month)2);
+            }
+
+
+
+
+            Console.ReadKey();
+        
+        } 
+           
+
+
+    
+        
+        
+    }
+}
---
+++ Enum_Struct/Enum_Struct_Test/Struct_Test/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Test
+{
+    internal class Program
+    {
+        struct SinhVien
+        {
+            public string maSV;
+            public string tenSV;
+            public string lop;
+            public double diem1;
+            public double diem2;
+            
+        }
+         static void NhapSV(out SinhVien SV)
+        {
+            Console.WriteLine("Nhap ma sinh vien: ");
+            SV.maSV =Console.ReadLine();
+            Console.WriteLine("Nhap ten sinh vien: ");
+            SV.tenSV =Console.ReadLine();
+            Console.WriteLine("Nhap lop sinh vien: ");
+            SV.lop=Console.ReadLine();
+            Console.WriteLine("Nhap diem mon 1 sinh vien: ");
+            SV.diem1 =double.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap diem mon 2 sinh vien: ");
+            SV.diem2 = double.Parse(Console.ReadLine());
+            
+        }
+        static double  diemTBsinhvien(SinhVien SV)
+        {
+            return (SV.diem1 + SV.diem2) / 2;
+        }
+
+        static void XuatSV(SinhVien SV)
+        {
+            Console.WriteLine($"{SV.maSV}_{SV.tenSV}_{SV.lop}_{SV.diem1}_{SV.diem2}");
+        }
+        static void Main(string[] args)
+        {
+            SinhVien SV1=new SinhVien();
+            NhapSV(out SV1);
+            XuatSV(SV1);
+            Console.WriteLine($"Diem trung binh sinh vien la: {diemTBsinhvien(SV1)}");
+        }
+    }
+}
