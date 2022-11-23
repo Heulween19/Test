145b49198fc6e43b3a9619043ad13f23a7cc2499
Heulween19 <103297323+Heulween19@users.noreply.github.com>
2022-11-23 14:05:43 +0700

    add constructor

---
+++ Constructor_Test/Construcotr_Test_1/Construcotr_Test_1/Customer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Construcotr_Test_1
+{
+    internal class Customer
+    {
+        private string CustomerID;
+
+        private string CustomerName;
+        
+
+        public Customer(string iD,string name)
+        {
+            this.CustomerID=iD;
+            this.CustomerName=name; 
+        }
+        public string getCustomerID()
+        {
+            return CustomerID;
+        }
+        public void setCustomerID(string newID)
+        {
+            if(CustomerID==null && CustomerID.Length>=10)
+            {
+                Console.WriteLine("ID không hợp lệ!");
+                Console.WriteLine("Nhập ID: ");
+                CustomerID =Console.ReadLine();
+            }
+            else
+            {
+                CustomerID = newID;
+            }
+        }
+
+        public string getCustomerName()
+        {
+            return CustomerName;
+        }
+        public void setCustomerName( string newName)
+        {
+            if (CustomerName == null && CustomerName.Length >= 50)
+            {
+                Console.WriteLine("Tên không hợp lệ!");
+                Console.WriteLine("Nhập tên: ");
+                CustomerName = Console.ReadLine();
+            }
+            else
+            {
+                CustomerName = newName;
+            }
+        }
+
+       
+    }
+}
---
+++ Constructor_Test/Construcotr_Test_1/Construcotr_Test_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Construcotr_Test_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding=Encoding.UTF8;
+            Console.OutputEncoding=Encoding.UTF8;
+
+            Customer Huyen = new Customer("MS001", "HUyền");
+            Console.WriteLine(Huyen.getCustomerID());
+            Console.WriteLine(Huyen.getCustomerName());
+            Huyen.setCustomerID("MS1902");
+            Console.WriteLine("ID mới: "+ Huyen.getCustomerID());
+        }
+    }
+}
